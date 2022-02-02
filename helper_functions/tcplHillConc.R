322c5d6f4389d57cebb1e2a3c154235807546b65
Messier <messierkp@almbp02121578.niehs.nih.gov>
2022-02-02 10:34:05 -0500

    restoring to versions from tcpl github

--- helper_functions/tcplHillConc.R
+++ helper_functions/tcplHillConc.R
@@ -2,16 +2,8 @@
 # tcplHillConc: Calculate the concentration for a given value
 #-------------------------------------------------------------------------------
 
-#' @rdname hill_utils
-#' @export
 
 tcplHillConc <- function(val, tp, ga, gw, bt = 0) {
-  
-  val <- rep(val,length(tp))
-  
-  idx <- val > tp
-  
-  val[idx] <- tp[idx] - 1e-10
   
   ga - log10((tp - bt)/(val - bt) - 1)/gw
 
--- helper_functions/tcplHillVal.R
+++ helper_functions/tcplHillVal.R
@@ -1,3 +1,8 @@
+#-------------------------------------------------------------------------------
+# tcplHillVal: Calculate the efficacy for a given concentration (log-concentration)
+#-------------------------------------------------------------------------------
+
+
 tcplHillVal <- function(logc, tp, ga, gw, bt = 0) {
   
   bt + (tp - bt)/(1 + 10^((ga - logc)*gw))
