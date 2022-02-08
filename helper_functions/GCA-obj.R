f9b4482eec4859f71723092c04085e77b938619b
Messier <messierkp@almbp02121578.niehs.nih.gov>
2022-02-08 11:42:08 -0500

    change to optimizing on the natural log scale - better precision in estimate

--- helper_functions/GCA-obj.R
+++ helper_functions/GCA-obj.R
@@ -1,11 +1,15 @@
-GCA.obj <- function(E,Ci,tp,AC50){
+GCA.obj <- function(effic,Ci,tp,AC50){
   # Generalized concentration addition objective function
   # Trying to find the optimal efficacy value, E
   # Based on a regular space AC50 and concentrations 
   
+  # Solving for the efficacy on the natural log-scale. This allows for 
+  # better precision in the low values, e.g. 1 x 10-5
+  E <- exp(effic)
   ECi <- tcplHillConc_v2(E,tp,AC50,rep(1,length(tp)))
   gca.val <- sum(Ci/ECi,na.rm = F)
   val <- (gca.val - 1)^2
+  # print(val)
   return(val)
   
 }
