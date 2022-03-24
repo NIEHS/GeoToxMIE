551ff23c75b67734a8584574a1da3346807c08af
Kristin Eccles <kristin.eccles@gmail.com>
2022-03-24 16:03:25 -0400

    updated ploting size and configuration

--- 01-CYP1A1-figures.R
+++ 01-CYP1A1-figures.R
@@ -1,5 +1,5 @@
 ######################################################
-# NATA and Tox21
+# Manuscript Figure 3 
 # By: Kristin Eccles
 # Date: May7th, 2021
 # Written in R Version 4.0.2
@@ -15,6 +15,7 @@ library(sjPlot)
 library(stringr)
 library(maps)
 library(ggpubr)
+library(forcats)
 
 # Import data
 hill2.fit <- get(load("/Volumes/SHAG/GeoTox/data/Hill_2param_model_fit.RData"))
@@ -22,52 +23,46 @@ hill2.fit <- get(load("/Volumes/SHAG/GeoTox/data/Hill_2param_model_fit.RData"))
 #NATA
 nata_df<- read.csv("/Volumes/SHAG/GeoTox/data/2014_NATA_CONCS.csv")
 nata_chemicals <- read.csv("/Volumes/SHAG/GeoTox/data/NATA_pollutant_names_casrn.csv")
-county_2014 <-st_read("/Volumes/SHAG/GeoTox/data/cb_2014_us_county_5m/cb_2014_us_county_5m.shp")
 
-# There were some repeats of reading in data?!
+
+ice_data <- get(load("/Volumes/SHAG/GeoTox/data//210105_ICE_cHTS_invitrodbv33.Rdata"))
+ice_data <- subset(ice_data, new_hitc == 1)
+states <- st_as_sf(map("state", plot = FALSE, fill = TRUE))
+
+county_2014 <-st_read("/Volumes/SHAG/GeoTox/data/cb_2014_us_county_5m/cb_2014_us_county_5m.shp")
+county_2014$GEOID <- as.numeric(county_2014$GEOID)
+
+#limit to continental USA
+county_2014<- subset(county_2014,  STATEFP != "02" )
+county_2014<- subset(county_2014,  STATEFP != "15" )
+county_2014<- subset(county_2014,  STATEFP != "60" )
+county_2014<- subset(county_2014,  STATEFP != "66" )
+county_2014<- subset(county_2014,  STATEFP != "69" )
+county_2014<- subset(county_2014,  STATEFP != "72" )
+county_2014<- subset(county_2014,  STATEFP != "78" )
+county_2014$countyid <-as.numeric(paste0(county_2014$STATEFP, county_2014$COUNTYFP))
+
+###############################################################################
+# Determine what the overlap between NATA chemicals and tox21 chemicals
 #simplify names
 nata_chemicals$web_name <- str_to_title(nata_chemicals$web_name, locale = "en")
 nata_chemicals$web_name <- str_replace(nata_chemicals$web_name , " \\s*\\([^\\)]+\\)", "")
 
-# aggregate to county
-nata_county_mean <- aggregate(nata_df[,7:ncol(nata_df)] , by=list(nata_df$STCOFIPS) , FUN=mean) 
-nata_county_sd <- aggregate(nata_df[,7:ncol(nata_df)] , by=list(nata_df$STCOFIPS) , FUN=sd) 
+nata_tox21 <- merge(ice_data, nata_chemicals,  by.x = "casn", by.y = "casrn") # n = 3533
+nata_tox21$presence <- "1" 
+length(unique(nata_tox21$aenm)) # 643 assays
 
-#melt
-county_stack_mean <- melt(nata_county_mean, id=1)
-colnames(county_stack_mean) <- cbind("FIPS", "chemical", "concentration_mean")
+nata_tox21_unique <- nata_tox21[!duplicated(nata_tox21$chnm), ]
+length(nata_tox21_unique$chnm) # n= 83 unique chemicals
 
-county_stack_sd <- melt(nata_county_sd, id=1)
-colnames(county_stack_sd) <- cbind("FIPS", "chemical", "concentration_sd")
-
-county_stack <- left_join(county_stack_mean, county_stack_sd, by=c("FIPS","chemical"), keep= FALSE)
-
-# add casrn
-county_stack <- left_join(county_stack, nata_chemicals, by=c("chemical" = "smoke_name"), keep= FALSE)
-
-# Create a STATE ID 
-county_stack$STATE <- county_stack$FIPS %>% as.character()
-county_stack$STATE[nchar(county_stack$STATE)==4] <- str_pad(substr(county_stack$STATE[nchar(county_stack$STATE)==4],1,1),
-                                                            width = 2,side = "left",pad = "0")
-county_stack$STATE[nchar(county_stack$STATE)==5] <- substr(county_stack$STATE[nchar(county_stack$STATE)==5],1,2)
-
-
-#limit to continental USA
-county_stack<- subset(county_stack,  STATE != "02" )
-county_stack<- subset(county_stack,  STATE != "15" )
-county_stack<- subset(county_stack,  STATE != "60" )
-county_stack<- subset(county_stack,  STATE != "66" )
-county_stack<- subset(county_stack,  STATE != "69" )
-county_stack<- subset(county_stack,  STATE != "72" )
-county_stack<- subset(county_stack,  STATE != "78" )
-
-### filter county_stack by the 41 chemicals in the analysis 
-
-county_stack_filter <- filter(county_stack,casrn %in% hill2.fit$casn)
-
-################################################################################
-#### Join with the CYP1A1 2-parameter hill model fit data ####
-county_cyp1a1_up <- left_join(county_stack_filter, hill2.fit, by=c("casrn" = "casn"), keep= FALSE)
+# summarized number of chemicals by assay
+nata_tox21_count <- as.data.frame(nata_tox21) %>%
+  group_by(nata_tox21$aenm) %>%
+  summarise(count = n())
+nata_tox21_count <- as.data.frame(nata_tox21_count[,1:2])
+colnames(nata_tox21_count) <- cbind("assay name", "chemical_count")
+nata_tox21_count
+#write.csv(nata_tox21_count, "nata_tox21_count.csv")
 
 # aggregate to county
 nata_county <- aggregate(nata_df[,7:ncol(nata_df)] , by=list(nata_df$STCOFIPS) , FUN=mean) 
@@ -91,6 +86,7 @@ concentration_norm=unsplit(lapply(split(as.data.frame(nata_county_stack$concentr
 colnames(concentration_norm) <- "concentration_norm"
 nata_county_stack <- cbind(nata_county_stack, concentration_norm)
 
+
 # limit to chemicals avaliable in httk
 
 in.chems <-  c("98-86-2","92-87-5","92-52-4","117-81-7","133-06-2","532-27-4","133-90-4","57-74-9","510-15-6","94-75-7" ,
@@ -101,60 +97,63 @@ in.chems <-  c("98-86-2","92-87-5","92-52-4","117-81-7","133-06-2","532-27-4","1
 
 nata_county_stack <-nata_county_stack[(nata_county_stack$casrn %in% in.chems),]
 
-################################################################################
-#### CYP1A1 ####
-hill2.fit$chemical_name <- rownames(hill2.fit)
-hill2.fit$aenm <- "LTEA_HepaRG_CYP1A1_up"
+# subset tox21 data
+ice_cyp1a1_up <- subset(ice_data, aenm == "LTEA_HepaRG_CYP1A1_up" )
 
+nata_county_cyp1a1_up <- left_join(nata_county_stack, ice_cyp1a1_up, by=c("casrn" = "casn"), keep= FALSE)
+nata_county_cyp1a1_up<- nata_county_cyp1a1_up[!is.na(nata_county_cyp1a1_up$ACC), ]
+nata_county_cyp1a1_up<- nata_county_cyp1a1_up[!is.na(nata_county_cyp1a1_up$concentration), ]
+nata_county_cyp1a1_up <-  mutate(nata_county_cyp1a1_up, 
+                                 count = ifelse(concentration >0, 1, 0))
 #### Heatmap ####
-heatmap_df<- as.data.frame(hill2.fit) %>%
-  group_by(aenm, chemical_name)%>%
-  summarise(mean=mean(10^logAC50))%>%
+heatmap_df<- as.data.frame(nata_county_cyp1a1_up) %>%
+  group_by(aenm, chnm) %>%
+  summarise(mean=mean(AC50))%>%
   as.data.frame()
-heatmap_df
 summary(heatmap_df$mean)
 
-heatmap_cyp1a1_up <- ggplot(heatmap_df, aes(x = fct_reorder(chemical_name, mean),y = aenm,  fill= mean)) + 
+heatmap_cyp1a1_up <- ggplot(heatmap_df, aes(x = fct_reorder(chnm, mean),y = aenm,  fill= mean)) + 
   geom_tile()+
   theme_bw()+
   scale_fill_viridis_c(direction = 1,option = "A") +
-  labs(y="Assay", x="Chemical", fill = "AC50 (μM)")+
-  theme(axis.text.x = element_text(angle = 90, hjust=1), 
+  labs(y="Assay", x="Chemical", fill = "EC50 (μM)")+
+  theme(axis.text.x = element_text(angle = 45, hjust=1), 
         axis.text.y = element_blank(), 
         axis.title.y = element_blank(), 
         axis.ticks.y = element_blank(), 
-        axis.ticks.x = element_blank())+
-  theme(text = element_text(size = 8), aspect.ratio=3/10)
+        legend.title=element_text(size=14),
+        text = element_text(size = 18), 
+        aspect.ratio=1.5/10,
+        plot.margin = margin(10, 10, 10, 100))
 heatmap_cyp1a1_up
 #save_plot("heatmap_cyp1a1_up.tif", heatmap_cyp1a1_up, width = 40, height = 30, dpi = 200)
 
 ##### Facet Wrap Map ####
-cyp1a1_up_nata_county_sp <- left_join(county_2014, nata_county_stack, by=c("countyid" = "FIPS"), keep=FALSE)
+cyp1a1_up_nata_county_sp <- left_join(county_2014, nata_county_cyp1a1_up, by=c("countyid" = "FIPS"), keep=FALSE)
 cyp1a1_up_nata_county_sf <-st_as_sf(cyp1a1_up_nata_county_sp)
 
 cyp1a1_up_all_map <- ggplot(data = cyp1a1_up_nata_county_sf, aes(fill=concentration_norm)) +
   geom_sf(lwd = 0)+
-  facet_wrap(vars(web_name), ncol=6, nrow=7)+
+  facet_wrap(vars(chnm), ncol=7)+
   theme_bw()+
   geom_sf(data = subset(cyp1a1_up_nata_county_sf, concentration == 0), aes(fill=concentration), fill = "light grey", size=0)+
   geom_sf(data = states, fill = NA, size=0.15)+
   scale_fill_viridis_c(name = "Normalized Concentration", direction = -1,option = "A") +
   theme(axis.ticks.x=element_blank(), axis.title.x=element_blank(), axis.text.x=element_blank(),
         axis.ticks.y=element_blank(), axis.title.y=element_blank(), axis.text.y=element_blank(),
-        legend.text=element_text(angle = 45), legend.title=element_text(size=12), 
+        legend.text=element_text(angle = 45),
         legend.position="bottom", 
-        strip.text = element_text(size = 6))
-cyp1a1_up_all_map
+        strip.text = element_text(size = 10),
+        text = element_text(size = 18))
+
 #cyp1a1_up_all_map
 #save_plot("facet_cyp1a1_up_nata.tif", cyp1a1_up_all_map, width = 40, height = 30, dpi = 200)
 
 
 #### Count Map ####
-nata_county_stack <-  mutate(nata_county_stack, 
-                                 count = ifelse(concentration >0, 1, 0))
 # cyp1a1_up composite count
-count_county_cyp1a1_up <- as.data.frame(nata_county_stack) %>%
-  group_by(FIPS) %>%
+count_county_cyp1a1_up <- as.data.frame(nata_county_cyp1a1_up) %>%
+  group_by(nata_county_cyp1a1_up$FIPS) %>%
   summarise(sum = sum(count))
 count_county_cyp1a1_up <- as.data.frame(count_county_cyp1a1_up)
 colnames(count_county_cyp1a1_up) <- cbind("FIPS", "count")
@@ -168,14 +167,17 @@ count_cyp1a1_up_map <- ggplot(data = count_county_cyp1a1_up_sf, aes(fill=count))
   theme_bw()+
   geom_sf(data = states, fill = NA, size=0.15)+
   scale_fill_viridis_c(name="# Chemicals", direction = -1,option = "A") +
-  theme(text = element_text(size = 12), axis.ticks.x = element_blank(),
-        axis.text.x = element_blank(), axis.ticks.y = element_blank(),
+  theme(axis.ticks.x = element_blank(),
+        axis.text.x = element_blank(), 
+        axis.ticks.y = element_blank(),
         axis.text.y = element_blank(), 
-        legend.position="bottom")
+        legend.position="right", 
+        text = element_text(size = 18), 
+        aspect.ratio=5/10)
 count_cyp1a1_up_map
-#save_plot("count_cyp1a1_up_uM_sumACC.tif", count_cyp1a1_up_map, width = 40, height = 30, dpi = 200)
+#save_plot("count_cyp1a1_up_uM_sumAC50.tif", count_cyp1a1_up_map, width = 40, height = 30, dpi = 200)
 
-#### Combine Plots ####
+#### Combine Plots H ####
 cyp1a1_up1=ggarrange(count_cyp1a1_up_map, heatmap_cyp1a1_up, 
                      labels = c("B", "C"),
                      #vjust = -0.005,
@@ -190,9 +192,32 @@ cyp1a1_up=ggarrange(cyp1a1_up_all_map, cyp1a1_up1,
                     vjust = 1,
                     #hjust = -0.5,
                     ncol = 2, nrow = 1,
-                    widths = c(1, 0.5),
+                    #widths = c(1, 0.5),
                     font.label = list(size = 16, color = "black", face = "bold"),
                     common.legend = FALSE)
 
 #Plot figures with dpi=300
-save_plot("/Volumes/SHAG/GeoTox/data/plots/cyp1a1_up_figure2.tif", cyp1a1_up, width = 32, height = 20, dpi = 300)
+save_plot("cyp1a1_up_3_figure2.tif", cyp1a1_up, width = 45, height = 40, dpi = 300)
+
+
+#### Combine Plots V ####
+cyp1a1_v=ggarrange(cyp1a1_up_all_map, count_cyp1a1_up_map, heatmap_cyp1a1_up, 
+                     labels = c("A", "B", "C"),
+                     #vjust = -0.005,
+                     #hjust = -0.5,
+                     widths = c(1, 0.65, 0.5),
+                     ncol = 1, nrow = 3,
+                     heights = c(1, 0.65, 0.5), 
+                     align = "v",
+                     font.label = list(size = 20, color = "black", face = "bold"),
+                     common.legend = FALSE)
+
+
+
+#Plot figures with dpi=300
+save_plot("/Volumes/SHAG/GeoTox/data/plots/Figure3_v20220324.tif", cyp1a1_v, width = 36, height = 50, dpi = 300)
+
+
+
+
+
