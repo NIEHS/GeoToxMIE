58ee8418e8a3c9edc675e4411503f3e558517c57
Kristin Eccles <kristin.eccles@gmail.com>
2022-04-18 10:40:08 -0400

    updated independent action equation

--- helper_functions/IA-Pred.R
+++ helper_functions/IA-Pred.R
@@ -1,8 +1,9 @@
-IA.pred <- function(Ci,tp,AC50,slope = 1){
+IA.pred <- function(Ci,tp,AC50,Emax,slope = 1){
   # Independent action prediction from the tcpl Hill function
   
   E <- tcplHillVal_v2(Ci,tp,AC50,slope)
   
-  1 - prod( (1 - E) )
+  Emax*(1 - prod( (1 - E/Emax) ))
+  # 
   
 }
