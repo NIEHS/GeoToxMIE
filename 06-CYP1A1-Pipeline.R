7274431704527672a0dcccebd65a28aa1501f5d5
Kristin Eccles <kristin.eccles@gmail.com>
2022-04-29 14:34:27 -0400

    updated figure panels

--- 06-CYP1A1-Pipeline.R
+++ 06-CYP1A1-Pipeline.R
@@ -19,7 +19,7 @@ library(reshape)
 library(scales)
 
 # load data
-load("/Volumes/SHAG/GeoTox/data/final_response_by_county_202200415.RData")
+load("/Volumes/SHAG/GeoTox/data/final_response_by_county_20220422.RData")
 
 load ("/Volumes/SHAG/GeoTox/data/FIPS_by_county.RData")
 FIPS <- as.data.frame(FIPS)
@@ -58,17 +58,13 @@ ivive.summary.df <- response.by.county %>%
             x95_quantile = quantile(value, 0.95, na.rm = TRUE),
             x5_quantile = quantile(value, 0.05, na.rm = TRUE))%>%
             as.data.frame()
-ivive.summary.df
 
-
-
-mean(subset(ivive.summary.df, health_measure == "GCA.HQ.10")$median >1)*100
-mean(subset(ivive.summary.df, health_measure == "GCA.HQ.10")$x5_quantile >1)*100
-mean(subset(ivive.summary.df, health_measure == "GCA.HQ.10")$x95_quantile >1)*100
+mean(subset(ivive.summary.df, health_measure == "HQ.10")$median >1)*100
+mean(subset(ivive.summary.df, health_measure == "HQ.10")$x5_quantile >1)*100
+mean(subset(ivive.summary.df, health_measure == "HQ.10")$x95_quantile >1)*100
 
 # add FIPS
 ivive.summary.df <- left_join(ivive.summary.df, FIPS, by="id", keep=FALSE)
-#write.csv(ivive.summary.df, "/Volumes/SHAG/GeoTox/data/mc_all_summary_df_20220415.csv")
 
 # make data spatial
 ivive.summary.df_stack <- melt(ivive.summary.df[,2:ncol(ivive.summary.df)], id.vars=c("FIPS", "health_measure"))
@@ -86,8 +82,8 @@ summary_stats
 write.csv(summary_stats, "summary_stats.csv")
 
 
-dose.labs <- c("GCA Response", "IA Response", "GCA RQ", "IA RQ")
-names(dose.labs) <- c("GCA.Eff", "IA.eff", "GCA.HQ.10", "IA.HQ.10")
+dose.labs <- c("CA Response", "IA Response", "RQ")
+names(dose.labs) <- c("GCA", "IA", "HQ.10")
 
 supp.labs <- c("5th Percentile", "Median", "95th Percentile")
 names(supp.labs) <- c("x5_quantile", "median", "x95_quantile")
@@ -100,74 +96,55 @@ histogram_HM <- ggplot(data=ivive.summary.df_stack, aes(x=log10(value)))+
   ylab("Count")+
   xlab("Log10 Risk Metric Value")
 histogram_HM
-save_plot("/Volumes/SHAG/GeoTox/data/plots/health_metric_histogram_20220415.tif", histogram_HM, width = 30, height = 30, dpi = 200)
+save_plot("/Volumes/SHAG/GeoTox/data/plots/health_metric_histogram_20220422.tif", histogram_HM, width = 30, height = 30, dpi = 200)
 
 
 
 
 ################################################################################################
 #### Multi Panel Plots ####
-#GCA.Eff, IA.eff, GCA.HQ.10, IA.HQ.10
+#GCA.Eff, IA.eff, IA.HQ.10
 ivive_county_cyp1a1_up_sp<- left_join(county_2014, ivive.summary.df_stack, by=c("countyid" = "FIPS"), keep=FALSE)
 ivive_county_cyp1a1_up_sf <-st_as_sf(ivive_county_cyp1a1_up_sp)
 
 plot.labs <- as_labeller(c(`x5_quantile` = "5th Percentile", `median` = "Median", `x95_quantile` = "95th Percentile"))
 
-GCA.Eff.plot <- ggplot(data = subset(ivive_county_cyp1a1_up_sf, health_measure == "GCA.Eff"), 
+GCA.Eff.plot <- ggplot(data = subset(ivive_county_cyp1a1_up_sf, health_measure == "GCA"), 
                        aes(fill=value))+
   geom_sf(lwd = 0)+
   facet_wrap(~variable, nrow=2, ncol=3, labeller = plot.labs)+
   theme_bw()+
   labs(fill="Sum")+
-  scale_fill_viridis_c(name = "Response (Log2 FC)",direction = -1,option = "A",trans = "sqrt",
+  scale_fill_viridis_c(name = "Predicted Response \nLog2 Fold Change \nmRNA Expression",direction = -1,option = "A",trans = "sqrt",
                        limits = c(0, 
-                                  max(subset(ivive_county_cyp1a1_up_sf, health_measure == "GCA.Eff")$value)),
-                       breaks = c(seq(min(subset(ivive_county_cyp1a1_up_sf, health_measure == "GCA.Eff")$value),
-                                      max(subset(ivive_county_cyp1a1_up_sf, health_measure == "GCA.Eff")$value),by = 0.25)),
+                                  max(subset(ivive_county_cyp1a1_up_sf, health_measure == "GCA")$value)),
+                       breaks = c(seq(min(subset(ivive_county_cyp1a1_up_sf, health_measure == "GCA")$value),
+                                      max(subset(ivive_county_cyp1a1_up_sf, health_measure == "GCA")$value),by = 0.5)),
                        label = function(x) sprintf("%.2f", x))+
   geom_sf(data = states, fill = NA, size=0.15)+
   theme(text = element_text(size = 12), legend.text=element_text(size = 8)) 
 GCA.Eff.plot
-save_plot("/Volumes/SHAG/GeoTox/data/plots/GCA_Eff_figureh_20220415.tif", GCA.Eff.plot, width = 40, height = 7, dpi = 200)
+#save_plot("/Volumes/SHAG/GeoTox/data/plots/GCA_Eff_figureh_20220428.tif", GCA.Eff.plot, width = 40, height = 7, dpi = 200)
 
 
-IA.Eff.plot <- ggplot(data = subset(ivive_county_cyp1a1_up_sf, health_measure == "IA.eff"), 
+IA.Eff.plot <- ggplot(data = subset(ivive_county_cyp1a1_up_sf, health_measure == "IA"), 
                        aes(fill=value))+
   geom_sf(lwd = 0)+
   facet_wrap(~variable, nrow=2, ncol=3, labeller = plot.labs)+
   theme_bw()+
   labs(fill="Sum")+
-  scale_fill_viridis_c(name = "Response (Log2 FC)",direction = -1,option = "A",trans = "sqrt",
+  scale_fill_viridis_c(name = "Predicted Response \nLog2 Fold Change \nmRNA Expression" ,direction = -1,option = "A",trans = "sqrt",
                        limits = c(NA, 
-                                  max(subset(ivive_county_cyp1a1_up_sf, health_measure == "IA.eff")$value)),
-                       breaks = c(seq(min(subset(ivive_county_cyp1a1_up_sf, health_measure == "IA.eff")$value),
-                                      max(subset(ivive_county_cyp1a1_up_sf, health_measure == "IA.eff")$value),by = 0.25)),
+                                  max(subset(ivive_county_cyp1a1_up_sf, health_measure == "IA")$value)),
+                       breaks = c(seq(min(subset(ivive_county_cyp1a1_up_sf, health_measure == "IA")$value),
+                                      max(subset(ivive_county_cyp1a1_up_sf, health_measure == "IA")$value),by = 0.5)),
                        label = function(x) sprintf("%.2f", x))+
   geom_sf(data = states, fill = NA, size=0.15)+
   theme(text = element_text(size = 12), legend.text=element_text(size = 8)) 
 IA.Eff.plot
-save_plot("/Volumes/SHAG/GeoTox/data/plots/IA_Eff_figureh_20220415.tif", IA.Eff.plot, width = 40, height = 7, dpi = 200)
+#save_plot("/Volumes/SHAG/GeoTox/data/plots/IA_Eff_figureh_20220428.tif", IA.Eff.plot, width = 40, height = 7, dpi = 200)
 
-GCA.HQ.10.plot <- ggplot(data = subset(ivive_county_cyp1a1_up_sf, health_measure == "GCA.HQ.10"), 
-                      aes(fill=value))+
-  geom_sf(lwd = 0)+
-  facet_wrap(~variable, nrow=2, ncol=3, labeller = plot.labs)+
-  theme_bw()+
-  labs(fill="Sum")+
-  scale_fill_viridis_c(name = "Risk Quotient",direction = -1,option = "A",trans = "sqrt",
-                       limits = c(0, 
-                                  round(max(subset(ivive_county_cyp1a1_up_sf, health_measure == "GCA.HQ.10")$value))), 
-                       breaks = c(round(min(subset(ivive_county_cyp1a1_up_sf, health_measure == "GCA.HQ.10")$value)),
-                                  10^seq(round(min(subset(ivive_county_cyp1a1_up_sf, health_measure == "GCA.HQ.10")$value)),
-                                         round(max(subset(ivive_county_cyp1a1_up_sf, health_measure == "GCA.HQ.10")$value)),by = 0.5)), 
-                       oob = scales::squish, 
-                       label = function(x) sprintf("%.1f", x))+
-  geom_sf(data = states, fill = NA, size=0.15)+
-  theme(text = element_text(size = 12), legend.text=element_text(size = 8)) 
-GCA.HQ.10.plot
-save_plot("/Volumes/SHAG/GeoTox/data/plots/GCA_HQ10_figureh_20220415.tif", GCA.HQ.10.plot, width = 40, height = 7, dpi = 200)
-
-HQ.1.10.plot <- ggplot(data = subset(ivive_county_cyp1a1_up_sf, health_measure == "GCA.HQ.10" & value >1), 
+HQ.1.10.plot <- ggplot(data = subset(ivive_county_cyp1a1_up_sf, health_measure == "HQ.10" & value >1), 
                          aes(fill=value))+
   geom_sf(data = states, fill = "light grey", size=0.15)+
   geom_sf(lwd = 0)+
@@ -178,10 +155,10 @@ HQ.1.10.plot <- ggplot(data = subset(ivive_county_cyp1a1_up_sf, health_measure =
   scale_fill_viridis_c(name = "RQ >1",direction = -1,option = "A")+
   theme(text = element_text(size = 12), legend.text=element_text(size = 8)) 
 HQ.1.10.plot
-save_plot("/Volumes/SHAG/GeoTox/data/plots/HQ10_1_figureh_20220415.tif", HQ.1.10.plot, width = 40, height = 7, dpi = 200)
+save_plot("/Volumes/SHAG/GeoTox/data/plots/HQ10_1_figureh_20220428.tif", HQ.1.10.plot, width = 20, height = 7, dpi = 200)
 
 
-IA.HQ.10.plot <- ggplot(data = subset(ivive_county_cyp1a1_up_sf, health_measure == "IA.HQ.10"), 
+HQ.10.plot <- ggplot(data = subset(ivive_county_cyp1a1_up_sf, health_measure == "HQ.10"), 
                          aes(fill=value))+
   geom_sf(lwd = 0)+
   facet_wrap(~variable, nrow=2, ncol=3, labeller = plot.labs)+
@@ -189,46 +166,24 @@ IA.HQ.10.plot <- ggplot(data = subset(ivive_county_cyp1a1_up_sf, health_measure 
   labs(fill="Sum")+
   scale_fill_viridis_c(name = "Risk Quotient",direction = -1,option = "A",trans = "sqrt",
                        limits = c(0, 
-                                  round(max(subset(ivive_county_cyp1a1_up_sf, health_measure == "IA.HQ.10")$value))), 
-                       breaks = c(round(min(subset(ivive_county_cyp1a1_up_sf, health_measure == "IA.HQ.10")$value)),
-                                  10^seq(round(min(subset(ivive_county_cyp1a1_up_sf, health_measure == "IA.HQ.10")$value)),
-                                         round(max(subset(ivive_county_cyp1a1_up_sf, health_measure == "IA.HQ.10")$value)),by = 0.5)), 
+                                  round(max(subset(ivive_county_cyp1a1_up_sf, health_measure == "HQ.10")$value))), 
+                       breaks = c(round(min(subset(ivive_county_cyp1a1_up_sf, health_measure == "HQ.10")$value)),
+                                  10^seq(round(min(subset(ivive_county_cyp1a1_up_sf, health_measure == "HQ.10")$value)),
+                                         round(max(subset(ivive_county_cyp1a1_up_sf, health_measure == "HQ.10")$value)),by = 0.5)), 
                        oob = scales::squish, 
                        label = function(x) sprintf("%.1f", x))+
   geom_sf(data = states, fill = NA, size=0.15)+
   theme(text = element_text(size = 12), legend.text=element_text(size = 8)) 
-IA.HQ.10.plot
-save_plot("/Volumes/SHAG/GeoTox/data/plots/IA_HQ10_figureh_20220415.tif", IA.HQ.10.plot, width = 40, height = 7, dpi = 200)
+HQ.10.plot
+#save_plot("/Volumes/SHAG/GeoTox/data/plots/HQ10_figureh_20220428.tif", IA.HQ.10.plot, width = 40, height = 7, dpi = 200)
 
-# 2x3 plot 
-GCA=ggarrange(GCA.Eff.plot , GCA.HQ.10.plot, 
-                     labels = c( "A", "B"),
+# 3x3 plot 
+composite_plot=ggarrange(GCA.Eff.plot , IA.Eff.plot, HQ.10.plot,
+                     labels = c( "A", "B", "C"),
                      vjust = 1,
                     align = "v",
                      #hjust = -0.5,
-                     ncol = 1, nrow = 2,
+                     ncol = 1, nrow = 3,
                      font.label = list(size = 20, color = "black", face = "bold"),
                      common.legend = FALSE)
-save_plot("/Volumes/SHAG/GeoTox/data/plots/GCA_composite_20220415.tif", GCA, width = 40, height = 15, dpi = 300)
-
-IA=ggarrange(IA.Eff.plot , IA.HQ.10.plot, 
-              labels = c( "A", "B"),
-              vjust = 1,
-              align = "v",
-              #hjust = -0.5,
-              ncol = 1, nrow = 2,
-              font.label = list(size = 20, color = "black", face = "bold"),
-              common.legend = FALSE)
-save_plot("/Volumes/SHAG/GeoTox/data/plots/IA_composite_20220415.tif", IA, width = 40, height = 15, dpi = 300)
-
-all=ggarrange(GCA.Eff.plot , GCA.HQ.10.plot, IA.Eff.plot , IA.HQ.10.plot, 
-             labels = c( "(A) Concentration Addition - Response", "(B) Concentration Addition - Hazard Quotient",
-                         "(C) Independent Action - Response", "(D) Indepdent Action - Harzard Quotient"),
-             align = "v",
-             #hjust = -0.5,
-             ncol = 1, nrow = 4,
-             font.label = list(size = 20, color = "black", face = "bold"),
-             common.legend = FALSE)
-save_plot("/Volumes/SHAG/GeoTox/data/plots/All_composite_20220415.tif", all, width = 40, height = 40, dpi = 300)
-
-
+save_plot("/Volumes/SHAG/GeoTox/data/plots/composite_plot_20220428.tif", composite_plot, width = 40, height = 25, dpi = 300)
--- 06-Sensitivity.R
+++ 06-Sensitivity.R
@@ -22,7 +22,7 @@ baseline <- get(load("/Volumes/SHAG/GeoTox/data/final_response_by_county_2022004
 sensitivity.conc.resp <- get(load("/Volumes/SHAG/GeoTox/data/sensitivity_results_conc_resp_20220415.RData"))
 
 
-#### Efficacy - GCA ####
+#### GCA ####
 sensitivity.GCA.Eff <- NULL
 for (x in 1:length(sensitivity.ext.conc)){
   print(x)
@@ -45,10 +45,10 @@ colnames(sensitivity.GCA.Eff) <- c("External Concentration","Toxicokinetic Param
 
 CR.melt <- melt(sensitivity.GCA.Eff)
 
-CR.melt$X2 <- factor(CR.melt$X2, levels = c( "External Concentration","Toxicokinetic Parameters",
+CR.melt$Var2 <- factor(CR.melt$Var2, levels = c( "External Concentration","Toxicokinetic Parameters",
                                                  "Concentration-Response","Age","Obesity", "Baseline"))
 
-conc.resp.plot.GCA <-ggplot(CR.melt, aes(x = value, y = as.factor(X2), fill = as.factor(X2))) +
+conc.resp.plot.GCA <-ggplot(CR.melt, aes(x = value, y = as.factor(Var2), fill = as.factor(Var2))) +
   stat_density_ridges( #bandwidth = 0.5
                       geom = "density_ridges_gradient", calc_ecdf = TRUE, 
                       quantiles = 4, quantile_lines = FALSE
@@ -56,17 +56,17 @@ conc.resp.plot.GCA <-ggplot(CR.melt, aes(x = value, y = as.factor(X2), fill = as
   scale_x_log10(labels = trans_format("log10", math_format(10^.x)))+
   scale_fill_viridis_d(option = "C")+
   theme(legend.position = "none")+
-  xlab("Median Log2 Fold Change mRNA Expression CYP1A1")+
+  xlab("Median Predicted Log2 Fold Change \nmRNA Expression CYP1A1")+
   ylab("Varying Parameter")+
   theme_minimal()+
   coord_cartesian(clip = "off")+
   theme(text = element_text(size = 14), legend.position="none", axis.text=element_text(size=14),
         axis.title=element_text(size=14)) 
 conc.resp.plot.GCA
-save_plot("/Volumes/SHAG/GeoTox/data/plots/Sensitivity_GCA_Eff__20220415.tif", conc.resp.plot.GCA,dpi = 200)
+#save_plot("/Volumes/SHAG/GeoTox/data/plots/Sensitivity_GCA_Eff__20220415.tif", conc.resp.plot.GCA,dpi = 200)
 
 
-#### Efficacy - IA model ####
+#### IA model ####
 sensitivity.IA.eff <- NULL
 for (x in 1:length(sensitivity.ext.conc)){
   print(x)
@@ -85,10 +85,10 @@ colnames(sensitivity.IA.eff) <- c("External Concentration","Toxicokinetic Parame
 
 CR.IA.melt <- melt(sensitivity.IA.eff)
 
-CR.IA.melt$X2 <- factor(CR.IA.melt$X2, levels = c( "External Concentration","Toxicokinetic Parameters",
+CR.IA.melt$Var2 <- factor(CR.IA.melt$Var2, levels = c( "External Concentration","Toxicokinetic Parameters",
                                              "Concentration-Response","Age","Obesity", "Baseline"))
 
-conc.resp.plot.IA <-ggplot(CR.IA.melt, aes(x = value, y = as.factor(X2), fill = as.factor(X2))) +
+conc.resp.plot.IA <-ggplot(CR.IA.melt, aes(x = value, y = as.factor(Var2), fill = as.factor(Var2))) +
   stat_density_ridges( #bandwidth = 0.5
     geom = "density_ridges_gradient", calc_ecdf = TRUE, 
     quantiles = 4, quantile_lines = FALSE
@@ -96,58 +96,17 @@ conc.resp.plot.IA <-ggplot(CR.IA.melt, aes(x = value, y = as.factor(X2), fill = 
   scale_x_log10(labels = trans_format("log10", math_format(10^.x)))+
   scale_fill_viridis_d(option = "C")+
   theme(legend.position = "none")+
-  xlab("Median Log2 Fold Change mRNA Expression CYP1A1")+
-  ylab("Varying Parameter")+
+  xlab("Median Predicted Log2 Fold Change \nmRNA Expression CYP1A1")+
+  ylab("")+
   theme_minimal()+
   coord_cartesian(clip = "off")+
   theme(text = element_text(size = 14), legend.position="none", axis.text=element_text(size=14),
-        axis.title=element_text(size=14)) 
+        axis.title=element_text(size=14), 
+        axis.text.y=element_blank()) 
 conc.resp.plot.IA
-save_plot("/Volumes/SHAG/GeoTox/data/plots/Sensitivity_IA_Eff__20220415.tif", conc.resp.plot.IA,  dpi = 200)
+#save_plot("/Volumes/SHAG/GeoTox/data/plots/Sensitivity_IA_Eff__20220415.tif", conc.resp.plot.IA,  dpi = 200)
 
-##### HQ - GCA model ####
-sensitivity.GCA.HQ.10<- NULL
-for (x in 1:length(sensitivity.ext.conc)){
-  print(x)
-  CR <- cbind(sensitivity.ext.conc[[x]]$GCA.HQ.10,sensitivity.httk[[x]]$GCA.HQ.10,
-              sensitivity.obesity[[x]]$GCA.HQ.10,sensitivity.age[[x]]$GCA.HQ.10,
-              sensitivity.conc.resp[[x]]$GCA.HQ.10,baseline[[x]]$GCA.HQ.10)
-  
-  sensitivity.GCA.HQ.10<- rbind(sensitivity.GCA.HQ.10,CR)
-  
-  
-}
-
-colnames(sensitivity.GCA.HQ.10) <- c("External Concentration","Toxicokinetic Parameters",
-                                     "Obesity","Age","Concentration-Response",
-                                     "Baseline")
-
-
-HQ.GCA.melt <- melt(sensitivity.GCA.HQ.10)
-
-HQ.GCA.melt$X2 <- factor(HQ.GCA.melt$X2, levels = c( "External Concentration","Toxicokinetic Parameters",
-                                                   "Concentration-Response","Age","Obesity", "Baseline"))
-
-HQ.plot.GCA <-ggplot(HQ.GCA.melt, aes(x = value, y = as.factor(X2), fill = as.factor(X2))) +
-  stat_density_ridges( #bandwidth = 0.5
-    geom = "density_ridges_gradient", calc_ecdf = TRUE, 
-    quantiles = 4, quantile_lines = FALSE
-  )  + 
-  scale_x_log10(labels = trans_format("log10", math_format(10^.x)))+
-  scale_fill_viridis_d(option = "C")+
-  theme(legend.position = "none")+
-  xlab("CYP1A1 Risk Quotient")+
-  ylab("Varying Parameter")+
-  theme_minimal()+
-  coord_cartesian(clip = "off")+
-  theme(text = element_text(size = 14), legend.position="none", axis.text=element_text(size=14),
-        axis.title=element_text(size=14)) 
-HQ.plot.GCA
-save_plot("/Volumes/SHAG/GeoTox/data/plots/Sensitivity_GCA_HQ_20220415.tif", HQ.plot.GCA, dpi = 200)
-
-
-
-#### HQ - IA model ####
+#### Risk Quotient ####
 sensitivity.IA.HQ.10 <- NULL
 for (x in 1:length(sensitivity.ext.conc)){
   print(x)
@@ -167,10 +126,10 @@ colnames(sensitivity.IA.HQ.10) <- c("External Concentration","Toxicokinetic Para
 
 HQ.IA.melt <- melt(sensitivity.IA.HQ.10)
 
-HQ.IA.melt$X2 <- factor(HQ.IA.melt$X2, levels = c( "External Concentration","Toxicokinetic Parameters",
+HQ.IA.melt$Var2 <- factor(HQ.IA.melt$Var2, levels = c( "External Concentration","Toxicokinetic Parameters",
                                                      "Concentration-Response","Age","Obesity", "Baseline"))
 
-HQ.plot.IA <-ggplot(HQ.IA.melt, aes(x = value, y = as.factor(X2), fill = as.factor(X2))) +
+HQ.plot.IA <-ggplot(HQ.IA.melt, aes(x = value, y = as.factor(Var2), fill = as.factor(Var2))) +
   stat_density_ridges( #bandwidth = 0.5
     geom = "density_ridges_gradient", calc_ecdf = TRUE, 
     quantiles = 4, quantile_lines = FALSE
@@ -178,36 +137,28 @@ HQ.plot.IA <-ggplot(HQ.IA.melt, aes(x = value, y = as.factor(X2), fill = as.fact
   scale_x_log10(labels = trans_format("log10", math_format(10^.x)))+
   scale_fill_viridis_d(option = "C")+
   theme(legend.position = "none")+
-  xlab("CYP1A1 Risk Quotient")+
-  ylab("Varying Parameter")+
+  xlab("Meidan CYP1A1 Risk Quotient")+
+  ylab(" ")+
   theme_minimal()+
   coord_cartesian(clip = "off")+
   theme(text = element_text(size = 14), legend.position="none", axis.text=element_text(size=14),
-        axis.title=element_text(size=14)) 
+        axis.title=element_text(size=14),
+        axis.text.y=element_blank()) 
 HQ.plot.IA
-save_plot("/Volumes/SHAG/GeoTox/data/plots/Sensitivity_IA_HQ_20220415.tif", HQ.plot.IA)
+#save_plot("/Volumes/SHAG/GeoTox/data/plots/Sensitivity_IA_HQ_20220415.tif", HQ.plot.IA)
 
 
 #### Combine Plots ####
-GCA=ggarrange(conc.resp.plot.GCA , HQ.plot.GCA, 
-              labels = c( "A", "B"),
+composite=ggarrange(conc.resp.plot.GCA , conc.resp.plot.IA , HQ.plot.IA, 
+              labels = c( "A", "B", "C"),
               vjust = 1,
-              align = "v",
+              align = "h",
               #hjust = -0.5,
-              ncol = 2, nrow = 1,
+              ncol = 3, nrow = 1,
+              widths = c(1, 1, 1), 
               font.label = list(size = 20, color = "black", face = "bold"),
               common.legend = FALSE)
-GCA
-save_plot("/Volumes/SHAG/GeoTox/data/plots/GCA_sensitivity_composite_20220415.tif", GCA, width = 40, height = 20, dpi = 300)
 
-IA=ggarrange(conc.resp.plot.IA , HQ.plot.IA, 
-              labels = c( "A", "B"),
-              vjust = 1,
-              align = "v",
-              #hjust = -0.5,
-              ncol = 2, nrow = 1,
-              font.label = list(size = 20, color = "black", face = "bold"),
-              common.legend = FALSE)
-IA
-save_plot("/Volumes/SHAG/GeoTox/data/plots/IA_sensitivity_composite_20220415.tif", IA, width = 40, height = 20, dpi = 300)
+save_plot("sensitivity_composite_20220428.tif", composite, width = 35, height = 20, dpi = 300)
+
 
